aac8146b3a957a3a943d09bc2e044006f8c14cf8
utsav <mori_utsav@rediffmail.com>
2017-09-01 17:07:36 +0530

    first commit

---
+++ MainActivity.java
@@ -0,0 +1,109 @@
+package com.example.utsav.bulksmsphone;
+
+
+import android.content.DialogInterface;
+import android.os.Environment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+
+public class MainActivity extends AppCompatActivity {
+    Button snd;
+    static EditText msg; File sdcard;
+    static TextView tv;
+    File file;BufferedReader br;
+
+    String line="";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        snd = (Button) findViewById(R.id.send);
+        msg = (EditText) findViewById(R.id.editText2);
+        tv = (TextView) findViewById(R.id.textView3);
+
+
+        snd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                alert.setTitle("Send Bulk Messages");
+                alert.setMessage("Are you sure you want to Send?");
+                alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                       sendmsg();
+
+                        dialog.dismiss();
+                    }
+                });
+
+                alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+                alert.show();
+
+
+
+            }
+        });
+    }
+
+    void sendmsg(){
+        try {
+           sdcard = Environment.getExternalStorageDirectory();
+
+
+           file = new File(sdcard, "smsnumbers.txt");
+
+            StringBuilder text = new StringBuilder();
+
+
+                br = new BufferedReader(new FileReader(file));
+        }catch (Exception e) {
+            Toast.makeText(this,"File Not Found",Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+
+        try{
+                while ((line = br.readLine()) != null) {
+                    if (line.length() == 10) {
+                        for (int i = 0; i < 10; i++) {
+                            int tem = line.charAt(i);
+                            if (tem < 0 || tem > 9)
+                                break;
+                        }
+                    }
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage(line, null,msg.getText().toString(), null, null);
+
+                }
+        }catch(Exception e){
+                Toast.makeText(this,"File Not Found or message not send!!!",Toast.LENGTH_LONG).show();
+                return ;
+
+             }
+            tv.setText("Messages Sent Successfully");
+
+    }
+
+}
+
+
---
+++ README.md
@@ -0,0 +1,3 @@
+this app sends messages from your android phone to numbers which are written notepad file "smsnumbers.txt" which is stored in android phone's internal storage.dummy smsnumbers.txt file is given in Repository.
+
+Note: Some Devices have Permission issue please grant permission from settings
